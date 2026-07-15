5facfd720c859ecca6f69ef2179463db1284cc3e
Priyanshivastt-24 <priyanshi.vastt24@gmail.com>
2026-07-15 20:46:35 +0530

    feat: implement CSS scroll snapping slides on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,36 @@
 import { useEffect } from 'react'
-import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Trusted from '../components/Trusted'
 import Categories from '../components/Categories'
 import WhyJobNest from '../components/WhyJobNest'
 import FeaturedJobs from '../components/FeaturedJobs'
-import Footer from '../components/Footer'
 
 export default function Home() {
     useEffect(() => {
+        document.documentElement.classList.add('snap-active');
         if ('scrollRestoration' in history) {
             history.scrollRestoration = 'manual';
         }
+        return () => {
+            document.documentElement.classList.remove('snap-active');
+        };
     }, []);
 
     return (
-        <>
-            <Hero />
-            <Trusted />
-            <Categories />
-            <WhyJobNest />
-            <FeaturedJobs />
-        </>
+        <div className="snap-container">
+            <div className="snap-slide">
+                <Hero />
+                <Trusted />
+            </div>
+            <div className="snap-slide">
+                <Categories />
+            </div>
+            <div className="snap-slide">
+                <WhyJobNest />
+            </div>
+            <div className="snap-slide">
+                <FeaturedJobs />
+            </div>
+        </div>
     )
 }
